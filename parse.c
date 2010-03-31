73e784a1cd85cadc67004a197c9a2995bc826c05
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2010-03-31 11:15:12 +0200

    Add handle_marker function.

--- parse.c
+++ parse.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<arpa/inet.h>
 
 typedef unsigned char  u8int;
 typedef unsigned short u16int;
@@ -18,6 +19,47 @@ struct {
 	u8int Nf;
 	struct Nf_array* nf_array;
 }start_of_frame;
+
+static void handle_marker(FILE* f, unsigned char marker) {
+	u16int length = 0;
+	switch (marker) {
+		case 0xc0:
+			/* first read the length */
+
+			printf("sizeof u16int: %u\n", sizeof(length));
+			fread(&length, sizeof(length), 1, f);
+			/* data with most significant bit first */
+			length = htons(length);
+			printf(" length: %u\n", length);
+
+			/* rewind a little bit */
+			fprintf(stdout, "before: %ld\n", ftell(f));
+			fseek(f, -sizeof(length), SEEK_CUR);
+			fprintf(stdout, "after: %ld\n", ftell(f));
+			if(ferror(f))
+				perror("fseek");
+#if 1
+
+			/* read all the frame */
+			fread(&start_of_frame, length, 1, f);
+#else
+			fread(&start_of_frame.length, sizeof(length), 1, f);
+			fread(&start_of_frame.sample, sizeof(u8int), 1, f);
+			fread(&start_of_frame.Y, sizeof(u16int), 1, f);
+			fread(&start_of_frame.X, sizeof(u16int), 1, f);
+			fread(&start_of_frame.Nf, sizeof(u8int), 1, f);
+#endif
+			printf(" length: %u\n", htons(start_of_frame.length));
+			printf(" sample: %u\n", start_of_frame.sample);
+			printf(" Y: %04x\n", htons(start_of_frame.Y));
+			printf(" X: %04x\n", htons(start_of_frame.X));
+			printf(" #components: %u\n", start_of_frame.Nf);
+			//printf(" id: %u\n", start_of_frame.nf_array[0].id);
+
+			break;
+	}
+
+}
 
 #define one_more_byte(c,f,idx) do{\
 		c = fgetc(f);\
@@ -59,6 +101,7 @@ int main(int argc, char* argv[]){
 
 			one_more_byte(c, fjpeg, idx);
 			printf("0xff%02x at %u", c, idx - 1);
+			handle_marker(fjpeg, c);
 			col = 0;
 		}else{
 
