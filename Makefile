4cd741453b7b4a4e5e7406d50e95e3fca3172775
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2010-04-02 11:07:56 +0200

    Add test for zigzag stuffs.

--- Makefile
+++ Makefile
@@ -1,12 +1,17 @@
 CPPFLAGS=-I. -Wall -Wpacked -Wpadded -g
 
-ALL = parse
+%: %.c
+	$(CC) $(CPPFLAGS) $^ -o $@ $(LDFLAGS)
+
+ALL = parse test_zig_zag
 
 all: $(ALL)
 
 parse: parse.o
+test_zig_zag.o: parse.c
+	$(CC) $(CPPFLAGS) -D__TEST_ZIG_ZAG $< -c -o $@
 
-parse.o: jpeg_header.h
+parse.o test_zig_zag.o: jpeg_header.h
 
 clean:
 	rm -f $(ALL) *.o core
--- parse.c
+++ parse.c
@@ -226,6 +226,19 @@ static unsigned int handle_marker(FILE* f, unsigned char marker) {
 
 
 int main(int argc, char* argv[]){
+#ifdef __TEST_ZIG_ZAG
+	u8int values[] = {
+		1,2,3,4,5,6,7,8,9,10,
+		11,12,13,14,15,16,17,18,19,20,
+		21,22,23,24,25,26,27,28,29,30,
+		31,32,33,34,35,36,37,38,39,40,
+		41,42,43,44,45,46,47,48,49,50,
+		51,52,53,54,55,56,57,58,59,60,
+		61,62,63,64
+	};
+	create_zig_zag(8);
+	de_zig_zag(values, 8);
+#else
 	if (argc < 2) {
 		fprintf(stderr, "usage: %s <jpeg file>\n", argv[0]);
 		exit(1);
@@ -266,5 +279,7 @@ int main(int argc, char* argv[]){
 
 	fclose(fjpeg);
 
+#endif
 	return 0;
 }
+
