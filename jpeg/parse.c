80818206da782237e54fb510fb1221a43914ba39
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2010-05-22 18:21:51 +0200

    Clean up.
    Eliminate some lines of code moved some time ago.

--- jpeg/parse.c
+++ jpeg/parse.c
@@ -31,116 +31,6 @@ static void read_length_and_rewind(u16int* length, FILE* f) {
 	fseek(f, -sizeof(*length), SEEK_CUR);
 	if(ferror(f))
 		perror("fseek");
-}
-
-#if 0
-#define printf_seed(seed) printf(" (%02u,%02u)", (seed).x, (seed).y)
-#else
-#define printf_seed(seed)
-#endif
-
-struct point {
-	unsigned int x;
-	unsigned int y;
-};
-
-struct point* zigzag = NULL;
-
-static int create_first_half_zigzag(unsigned int side) {
-	unsigned int idx = 0, diagonale, row;
-	/* who decides when y or x */
-	/*
-	 *	who =  1     X
-	 *	who = -1     Y
-	 * */
-	int who = 1;
-
-	struct point seed = {0, 0};
-
-	printf_seed(seed);
-	zigzag[idx] = seed;
-	idx++;
-
-
-	for (diagonale = 2 ; diagonale <= side ; diagonale++) {
-		seed.x = who > 0 ? seed.x + 1 : seed.x;
-		seed.y = who < 0 ? seed.y + 1 : seed.y;
-		zigzag[idx] = seed;
-
-		printf_seed(seed);
-		idx++;
-
-		/* this cycle doens't calculate the seed */
-		for(row = 1 ; row < diagonale ; row++) {
-			seed.x += -who;
-			seed.y +=  who;
-			printf_seed(seed);
-			zigzag[idx] = seed;
-
-			idx++;
-		}
-		who *= -1;
-	}
-
-	return who;
-}
-
-static void create_second_half_zigzag(unsigned int side, int who) {
-	/* idx is the number of element calculated yet */
-	unsigned int idx = (side * (side + 1))/2, diagonale, row;
-	struct point seed = zigzag[idx - 1];
-
-	for (diagonale = 1 ; diagonale < side ; diagonale++) {
-
-		seed.x = who < 0 ? seed.x+1 : seed.x;
-		seed.y = who > 0 ? seed.y+1 : seed.y;
-		zigzag[idx] = seed;
-
-		printf_seed(seed);
-		idx++;
-
-		/* this cycle doens't calculate the seed */
-		for(row = side - diagonale  - 1 ; row > 0 ; row--) {
-			seed.x += -who;
-			seed.y +=  who;
-			printf_seed(seed);
-			zigzag[idx] = seed;
-
-			idx++;
-		}
-		who *= -1;
-	}
-}
-
-static void create_zig_zag(unsigned int side) {
-	free(zigzag);
-	zigzag = malloc(sizeof(struct point) * side * side);
-	int who = create_first_half_zigzag(side);
-	create_second_half_zigzag(side, who);
-}
-
-static unsigned int from_col_row_to_idx(
-	unsigned int row,
-	unsigned int col,
-	unsigned int side)
-{
-	unsigned int cycle;
-	unsigned int max = side * (side + 1);
-	for (cycle = 0 ; cycle < max ; cycle++) {
-		struct point p = zigzag[cycle];
-		if (p.y == row && p.x == col)
-			return cycle;
-	}
-}
-
-static void de_zig_zag(u8int values[], unsigned int side) {
-	unsigned int col, row;
-	for (row = 0 ; row < side ; row++) {
-		for (col = 0 ; col < side ; col++) {
-			printf(" %02d",
-				values[from_col_row_to_idx(row, col, side)]);
-		}
-		printf("\n");
 	}
 }
 
@@ -220,49 +110,6 @@ static unsigned int read_quantization_table(FILE* f) {
 	return length;
 }
 
-/* algorithm from specification (pg55) */
-static unsigned int* generate_size_table(u8int bits[]) {
-	static unsigned int huffsize[256];
-	unsigned int k = 0, i = 1, j = 1;
-	while (i <= 16) {
-		if (j > bits[i]) {
-			i++;
-			j = 1;
-		} else {
-			huffsize[k] = i;
-			k++;
-			j++;
-		}
-	}
-
-	huffsize[k] = 0;
-
-	return huffsize;
-}
-
-/* TODO: rewrite better (maybe understand also what this means) */
-static unsigned int* generate_code_table(unsigned int huffsize[]) {
-	unsigned int k = 0, code = 0, si = huffsize[0];
-
-	static unsigned int huffcode[4096];
-
-	while (huffsize[k]) {
-		huffcode[k] = code;
-		code++;
-		k++;
-
-		if (!huffsize[k])
-			break;
-
-		while (huffsize[k] != si) {
-			code = code << 1;
-			si++;
-		}
-	}
-
-	return huffcode;
-}
-
 static unsigned int read_huffman_table(FILE* f) {
 	u16int length;
 	read_length_and_rewind(&length, f);
@@ -307,19 +154,6 @@ static unsigned int handle_marker(FILE* f, unsigned char marker) {
 
 
 int main(int argc, char* argv[]){
-#ifdef __TEST_ZIG_ZAG
-	u8int values[] = {
-		1,2,3,4,5,6,7,8,9,10,
-		11,12,13,14,15,16,17,18,19,20,
-		21,22,23,24,25,26,27,28,29,30,
-		31,32,33,34,35,36,37,38,39,40,
-		41,42,43,44,45,46,47,48,49,50,
-		51,52,53,54,55,56,57,58,59,60,
-		61,62,63,64
-	};
-	create_zig_zag(8);
-	de_zig_zag(values, 8);
-#else
 	if (argc < 2) {
 		fprintf(stderr, "usage: %s <jpeg file>\n", argv[0]);
 		exit(1);
@@ -365,7 +199,6 @@ int main(int argc, char* argv[]){
 
 	fclose(fjpeg);
 
-#endif
 	return 0;
 }
 
