4320ba25c950eb4d5853159d4b8b46726c81b30a
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2010-05-22 15:55:55 +0200

    Factorize code for zigzag operation.
    Also create a new source file to test this.

--- jpeg/ljpeg.c
+++ jpeg/ljpeg.c
@@ -1,7 +1,118 @@
 #include<jpeg/ljpeg.h>
 #include<huffman/huffman.h>
 
-huffman_t* huffman_from_jpeg_header(struct huffman_table t) {
+#if 0
+#define printf_seed(seed) printf(" (%02u,%02u)", (seed).x, (seed).y)
+#else
+#define printf_seed(seed)
+#endif
+
+struct point {
+	unsigned int x;
+	unsigned int y;
+};
+
+struct point* zigzag = NULL;
+
+static int create_first_half_zigzag(unsigned int side) {
+	unsigned int idx = 0, diagonale, row;
+	/* who decides when y or x */
+	/*
+	 *	who =  1     X
+	 *	who = -1     Y
+	 * */
+	int who = 1;
+
+	struct point seed = {0, 0};
+
+	printf_seed(seed);
+	zigzag[idx] = seed;
+	idx++;
+
+
+	for (diagonale = 2 ; diagonale <= side ; diagonale++) {
+		seed.x = who > 0 ? seed.x + 1 : seed.x;
+		seed.y = who < 0 ? seed.y + 1 : seed.y;
+		zigzag[idx] = seed;
+
+		printf_seed(seed);
+		idx++;
+
+		/* this cycle doens't calculate the seed */
+		for(row = 1 ; row < diagonale ; row++) {
+			seed.x += -who;
+			seed.y +=  who;
+			printf_seed(seed);
+			zigzag[idx] = seed;
+
+			idx++;
+		}
+		who *= -1;
+	}
+
+	return who;
+}
+
+static void create_second_half_zigzag(unsigned int side, int who) {
+	/* idx is the number of element calculated yet */
+	unsigned int idx = (side * (side + 1))/2, diagonale, row;
+	struct point seed = zigzag[idx - 1];
+
+	for (diagonale = 1 ; diagonale < side ; diagonale++) {
+
+		seed.x = who < 0 ? seed.x+1 : seed.x;
+		seed.y = who > 0 ? seed.y+1 : seed.y;
+		zigzag[idx] = seed;
+
+		printf_seed(seed);
+		idx++;
+
+		/* this cycle doens't calculate the seed */
+		for(row = side - diagonale  - 1 ; row > 0 ; row--) {
+			seed.x += -who;
+			seed.y +=  who;
+			printf_seed(seed);
+			zigzag[idx] = seed;
+
+			idx++;
+		}
+		who *= -1;
+	}
+}
+
+void create_zig_zag(unsigned int side) {
+	free(zigzag);
+	zigzag = malloc(sizeof(struct point) * side * side);
+	int who = create_first_half_zigzag(side);
+	create_second_half_zigzag(side, who);
+}
+
+static unsigned int from_col_row_to_idx(
+	unsigned int row,
+	unsigned int col,
+	unsigned int side)
+{
+	unsigned int cycle;
+	unsigned int max = side * (side + 1);
+	for (cycle = 0 ; cycle < max ; cycle++) {
+		struct point p = zigzag[cycle];
+		if (p.y == row && p.x == col)
+			return cycle;
+	}
+}
+
+void de_zig_zag(u8int values[], unsigned int side) {
+	unsigned int col, row;
+	for (row = 0 ; row < side ; row++) {
+		for (col = 0 ; col < side ; col++) {
+			printf(" %02d",
+				values[from_col_row_to_idx(row, col, side)]);
+		}
+		printf("\n");
+	}
+}
+
+huffman_t* huffman_from_jpeg_header(struct huffman_table* t) {
 	unsigned int nc_cycle, codeidx;
 
 	huffman_canon_t hc[0x100];
@@ -9,10 +120,10 @@ huffman_t* huffman_from_jpeg_header(struct huffman_table t) {
 
 	/* loop over the number of code of N nc_cycle bits */
 	for (nc_cycle = 0 ; nc_cycle < 16 ; nc_cycle++) {
-		for (codeidx = 0 ; codeidx < t.ncodes[nc_cycle] ; codeidx++) {
+		for (codeidx = 0 ; codeidx < t->ncodes[nc_cycle] ; codeidx++) {
 			hc[ncodes] = (huffman_canon_t) {
-				.symbol = t.values[ncodes],
-				.nbits  = nc_cycle
+				.symbol = t->values[ncodes],
+				.nbits  = nc_cycle + 1
 			};
 			ncodes++;
 		}
