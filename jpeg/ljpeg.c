77acef7cb6ee63f14000105f1226e64d7683655d
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2010-06-03 17:41:02 +0200

    Move quantization table related code.

--- jpeg/ljpeg.c
+++ jpeg/ljpeg.c
@@ -217,22 +217,12 @@ void JFIF_header_print_info(void) {
 			units[gJFIF_header->units]);
 }
 
-struct quantization_table* gquantization_table[2];
-
-void read_quantization_table_header(FILE* f) {
-	static int idx = 0;
-	gquantization_table[idx++] = section_to_buffer(f);
-}
-
-void quantization_table_print_info(void) {
-	create_zig_zag(8);
-	printf("FIRST QUANTIZATION TABLE\n");
-	ude_zig_zag(gquantization_table[0]->quantization[0].value, 8);
-	printf("SECOND QUANTIZATION TABLE\n");
-	ude_zig_zag(gquantization_table[1]->quantization[0].value, 8);
-}
-
 struct start_of_frame* gstart_of_frame = NULL;
+
+}
+
+}
+
 
 void read_start_of_frame(FILE* f) {
 	gstart_of_frame = section_to_buffer(f);
@@ -301,6 +291,23 @@ void ljpeg_print_huffman_tables(void) {
 	huffman_print(g_huffman_cbcr_ac);
 }
 
+struct quantization_table* gquantization_table[2];
+
+void read_quantization_table_header(FILE* f) {
+	static int idx = 0;
+	gquantization_table[idx++] = section_to_buffer(f);
+}
+
+void quantization_table_print_info(void) {
+	create_zig_zag(8);
+	printf("FIRST QUANTIZATION TABLE\n");
+	ude_zig_zag(gquantization_table[0]->quantization[0].value, 8);
+	if (gstart_of_frame->Nf < 3)
+		return;
+
+	printf("SECOND QUANTIZATION TABLE\n");
+	ude_zig_zag(gquantization_table[1]->quantization[0].value, 8);
+}
 
 struct start_of_scan g_start_of_scan;
 size_t g_scan_data_size;
